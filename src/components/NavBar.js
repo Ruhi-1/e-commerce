890fe07b92eb78c2fa003da0d08a570f5a562c04
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-02-29 18:15:31 -0600

    updated my cart button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/*
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
@@ -17,7 +18,7 @@ export default class Navbar extends Component {
                     <img src={logo} alt="store"
                     className="navbar-brand" />
                 </Link>
-                <ul classname="navbar-nav.align-items-center">
+                <ul className="navbar-nav.align-items-center">
                     <li className="nav-item ml-5">
                     <Link to="/" className="nav-link">products</Link>
                     </li>
@@ -30,26 +31,16 @@ export default class Navbar extends Component {
                         my cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size:1.4rem;
-background: transparent;
-border:0.05rem solid var(--lightBlue);
-color:var(--lightBlue);
-border-radius: 0.5rem;
-padding: 0.2rem 0.5rem;
-curser:pointer;
-margin:0.2rem 0.5rem 0.2rem 0;
-transition: all 0.5s ease-in-out;
-&:hover{
-    background:var(--lightBlue);
-    color:vr(--mainBlue);
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform: capitalize;
 }
-&:focus{
-    outline:none;
-}
-`;
+`
