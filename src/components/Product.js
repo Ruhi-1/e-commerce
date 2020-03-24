f7aebb727da89209b62826440ed2775a2693d26d
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-03-24 12:20:10 -0500

    solved the issue for product not beeing load

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,40 +3,41 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
 import propsTypes from 'prop-types';
-export default class Navbar extends Component {
+export default class Product extends Component {
     render() {
         const {id, title, img, price, inCart} = this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
                 <ProductConsumer>
-                    {value => (
-                        <div 
-                        className="img-container p-5" 
-                        onClick={() => 
-                            value.handleDetail(id)
-                        }>
-                        <Link to="/details">
-                            <img src={img} alt="product" className="card-img-tap" />
-                        </Link>
-                        <button 
-                        className="cart-btn" 
-                        disabled={inCart ? true : false} 
-                        onClick={() => {
-                            value.addToCart(id);
-                        }}
-                        >
-                            {inCart ? (
-                            <p className="text-capitalize mb-0" disabled>
-                                {" "}
-                                in cart
-                                    </p>
-                                    ):(
-                                    <i className="fas fa-cart-plus" />
-                                    )}
-                        </button>
-                    </div>
-                    )}
+                    {value => {
+                        return (
+                            <div 
+                                className="img-container p-5" 
+                                onClick={() => 
+                                value.handleDetail(id)
+                            }>
+                            <Link to="/details">
+                                <img src={img} alt="product" className="card-img-tap" />
+                            </Link>
+                            <button 
+                            className="cart-btn" 
+                            disabled={inCart ? true : false} 
+                            onClick={() => {
+                                value.addToCart(id);
+                            }}
+                            >
+                                {inCart ? (
+                                <p className="text-capitalize mb-0" disabled>
+                                    in cart
+                                        </p>
+                                        ):(
+                                        <i className="fas fa-cart-plus" />
+                                        )}
+                            </button>
+                        </div>
+                    )
+                }}
                 
                 
                 </ProductConsumer>
@@ -47,6 +48,7 @@ export default class Navbar extends Component {
                     </p>
                     <h5 className="text-blue font-italic mb-0">
                         <span className="mr-1">$</span>
+                        {price}
                     </h5>
                 </div>
                 </div>
