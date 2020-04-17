0bbbd673986f7a480f3fc5df8557d6e94b20aa5e
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-04-17 10:43:56 -0500

    added style, coun, and increment for the cartitems

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -22,7 +22,39 @@ export default class CartItem extends Component {
                         <span className="d-lg-none">price :</span> ${price}
                     </strong>
                 </div>
+                <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0 ">
+                    <div className="d-flex justify-content-center">
+                        <div>
+                            <span
+                                className="btn btn-black mx-1"
+                                onClick={() => {
+                                    return decrement(id);
+                                }}
+                            >
+                                -
+                            </span>
+                            <span className="btn btn-black mx-1">{count}</span>
+                            <span
+                                className="btn btn-black mx-1"
+                                onClick={() => {
+                                    return increment(id);
+                                }}
+                            >
+                                +
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            <div className="col-10 mx-auto col-lg-2 ">
+                <div className=" cart-icon" onClick={() => removeItem(id)}>
+                    <i className="fas fa-trash" />
+                </div>
             </div>
-        )
+
+            <div className="col-10 mx-auto col-lg-2 ">
+                <strong>item total : ${total} </strong>
+            </div>
+        </div>
+        );
     }
 }
