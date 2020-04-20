835e20d5c1dc2e44de8c8ced18a829097392eb7c
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-04-20 12:46:12 -0500

    added payapl button in carttotal.js

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import PaypalButton from "./PayPalButton";
+import PaypalButton from "./PayPalButton";
 import { Link } from "react-router-dom";
 
 export default class CartTotals extends Component {
