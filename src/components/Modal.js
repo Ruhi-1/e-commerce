bd8721cb38411d6b8d796d50cc1b37105db90413
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-04-24 11:22:54 -0500

    fixed the bug in modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -51,22 +51,22 @@ export default class Modal extends Component {
                     );
                 }
             }}
-      </ProductConsumer>
+        </ProductConsumer>
     );
   }
 }
 
 const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.3);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  #modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    #modal {
     background: var(--mainWhite);
-  }
+    }
 `;
