d98f0efe17d988919d4345a1efe7d890c3b9ba2b
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-04-24 11:39:20 -0500

    added modal file in app.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import Details from './components/Details';
 import ProductList from './components/ProductList';
 import Default from './components/Default';
 import Cart from './components/Cart/Cart';
-
+import Modal from "./components/Modal";
 class App extends Component {
   render() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={ProductList} />
-        <Route path="/details" component={Details} />
-        <Route path="/cart" component={Cart} />
-        <Route component={Default} />
-      </Switch>
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      <Modal />
     </React.Fragment>
     );
   }
