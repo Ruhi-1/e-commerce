c6e546ed86931f74cd27526930fbcabffe27f41c
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2020-05-05 10:47:55 -0500

    fixed bug in app.js and productlist.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -14,16 +14,16 @@ export default class ProductList extends Component {
                 <ProductWrapper className="py-5">
                     <div className="container">
                         <Title name="our" title="products" />
-                    <div className="row">
-                        {/* <ProductConsumer>
-                            {value => {
-                                return value.products.map(product => {
-                                    return <Product key={product.id} product={product} />;
-                                });
-                            }}
-                        </ProductConsumer> */}
+                        <div className="row">
+                            {/* <ProductConsumer>
+                                {value => {
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />;
+                                    });
+                                }}
+                            </ProductConsumer> */}
+                        </div>
                     </div>
-                </div>
                 </ProductWrapper>
             </React.Fragment>        
         );
